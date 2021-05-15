93421e7de9e2798c0e0b4b4279dc27a5d3b44265
Rick Spencer <richard.linger.spencer.3@gmail.com>
2021-05-15 12:40:22 -0400

    added fake user module

--- src/app.py
+++ src/app.py
@@ -1,14 +1,18 @@
 from flask import Flask
+import users
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
+    user = users.authorize_and_get_user()
+
     return (
         '<!DOCTYPE html>\n'
         '<meta charset="utf-8">\n<title>Plant Buddy</title>\n'
         '<img src="/static/logo.png">\n'
+        '<H3>Hello {}</H3\n'.format(user["user_name"])
     )
 
 if __name__ == '__main__':
---
+++ src/users.py
@@ -0,0 +1,2 @@
+def authorize_and_get_user():
+    return {"user_name": "rick"}
