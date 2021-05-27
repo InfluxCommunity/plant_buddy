ff29ef6c67bfd59f520b5bdce77598bc5a8a76ef
Rick Spencer <richard.linger.spencer.3@gmail.com>
2021-05-27 10:07:33 -0400

    deleted erroneous imports

--- src/app.py
+++ src/app.py
@@ -2,8 +2,6 @@ from flask import Flask, request, render_template
 
 import users
 import secret_store
-import influxdb_client
-import matplotlib.pyplot as plt, mpld3
 
 app = Flask(__name__)
 
@@ -30,7 +28,6 @@ def parse_line(line, user_name):
             "sensor_name" : sensor_names.get(line[2:4], "unkown"),
             "value" : line[4:],
             "user": user_name}
-
     return data
 
 if __name__ == '__main__':
